b156bf64eb830b0f0570b9b53ac429ae11459245
Chris Nero <chrisvn00@gmail.com>
2018-06-02 12:23:25 -0400

    Change char to unsigned char in DynBuffer

--- src/dynbuffer.c
+++ src/dynbuffer.c
@@ -15,24 +15,24 @@ void lv_buf_init(DynBuffer* self, size_t dataSize) {
 void lv_buf_push(DynBuffer* self, void* elem) {
     
     //tmp buffer, in case we need to realloc
-    //aliasing ftw
-    char tmp[self->dataSize];
+    //and elem aliases self->data
+    unsigned char tmp[self->dataSize];
     memcpy(tmp, elem, self->dataSize);
     if(self->len + 1 == self->cap) {
         self->data = lv_realloc(self->data, self->cap * 2 * self->dataSize);
         self->cap *= 2;
     }
-    void* off = ((char*)self->data) + (self->len++ * self->dataSize);
+    void* off = ((unsigned char*)self->data) + (self->len++ * self->dataSize);
     memcpy(off, tmp, self->dataSize);
 }
 
 void* lv_buf_pop(DynBuffer* self) {
     
     size_t off = --self->len * self->dataSize;
-    return ((char*)self->data) + off;
+    return ((unsigned char*)self->data) + off;
 }
 
 void* lv_buf_get(DynBuffer* self, size_t idx) {
     
-    return ((char*)self->data) + (idx * self->dataSize);
+    return ((unsigned char*)self->data) + (idx * self->dataSize);
 }
