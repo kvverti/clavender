ca34ece5d3860cec605c5f423ac13ef57732e81f
Chris Nero <chrisvn00@gmail.com>
2018-08-04 14:50:01 -0400

    lv_buf_push no longer always does an extra memcpy

--- src/dynbuffer.c
+++ src/dynbuffer.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 void lv_buf_init(DynBuffer* self, size_t dataSize) {
-    
+
     #define INIT_BUFFER_LEN 16
     self->dataSize = dataSize;
     self->data = lv_alloc(INIT_BUFFER_LEN * dataSize);
@@ -13,27 +13,30 @@ void lv_buf_init(DynBuffer* self, size_t dataSize) {
 }
 
 void lv_buf_push(DynBuffer* self, void* elem) {
-    
-    //tmp buffer, in case we need to realloc
-    //and elem aliases self->data
-    unsigned char tmp[self->dataSize];
-    memcpy(tmp, elem, self->dataSize);
+
     if(self->len + 1 == self->cap) {
+        //tmp buffer, in case
+        //elem aliases self->data
+        unsigned char tmp[self->dataSize];
+        memcpy(tmp, elem, self->dataSize);
         self->data = lv_realloc(self->data, self->cap * 2 * self->dataSize);
         self->cap *= 2;
+        void* off = ((unsigned char*)self->data) + (self->len++ * self->dataSize);
+        memcpy(off, tmp, self->dataSize);
+    } else {
+        void* off = ((unsigned char*)self->data) + (self->len++ * self->dataSize);
+        memcpy(off, elem, self->dataSize);
     }
-    void* off = ((unsigned char*)self->data) + (self->len++ * self->dataSize);
-    memcpy(off, tmp, self->dataSize);
 }
 
 void lv_buf_pop(DynBuffer* self, void* elem) {
-    
+
     void* off = ((unsigned char*)self->data) + (--self->len * self->dataSize);
     if(elem)
         memcpy(elem, off, self->dataSize);
 }
 
 void* lv_buf_get(DynBuffer* self, size_t idx) {
-    
+
     return ((unsigned char*)self->data) + (idx * self->dataSize);
 }
